7cd9028f4383a781d267bd43799e34eb6b4fb7a5
Alden  Sullivan <aw.sullivan17@gmail.com>
2023-05-12 14:40:22 -0400

    temp

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const fs = require('fs');
+// const Throttle = require('throttle');
+// const { ffprobeSync } = require('@dropb/ffprobe');
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req:any, res:any) => {
+	// const bitRate = ffprobeSync('../assets/power_chords.mp3').format.bit_rate;
+	// const throttle = new Throttle(bitRate / 8);
+	// const writables = [writable1];
+    const file = __dirname + '/power_chords.mp3';
+    fs.exists(file, (exists:any) => {
+        if (exists) {
+            const rstream = fs.createReadStream(file);
+            rstream.pipe(res);
+        } else {
+            res.send('Error - 404');
+            res.end();
+        }
+    });
+
+	// res.send('hello world')
+})
+
+app.listen(8080)
+
